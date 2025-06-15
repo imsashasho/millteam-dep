68562f37f5b91ea4fa92b8ebfb543d2e6c207de3
yatsinoa <imsashasho@gmail.com>
2025-06-15 12:04:29 +0300

    new

--- README.md
+++
@@ -1,1 +0,0 @@
-# millteam-dep
--- assets/scripts/about.bundle.js
+++ assets/scripts/about.bundle.js
@@ -20,13 +20,13 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _abo
 
 /***/ }),
 
-/***/ "./src/assets/scripts/about/cardsNumber.js":
-/*!*************************************************!*\
-  !*** ./src/assets/scripts/about/cardsNumber.js ***!
-  \*************************************************/
+/***/ "./src/assets/scripts/about/accordeon.js":
+/*!***********************************************!*\
+  !*** ./src/assets/scripts/about/accordeon.js ***!
+  \***********************************************/
 /***/ (function() {
 
-eval("// Select all the cards and the number container\nconst cards = document.querySelectorAll('.values-list-card');\nconst currentNumber = document.querySelector('.values-list-numbers-current .text-h4.text-pantone2');\n\n// Create an intersection observer\nconst observer = new IntersectionObserver(entries => {\n  entries.forEach(entry => {\n    if (entry.isIntersecting) {\n      // Get the index of the currently visible card\n      const index = Array.from(cards).indexOf(entry.target) + 1;\n\n      // Update the current number\n      currentNumber.textContent = `0${index}`; // Format as \"01\", \"02\", etc.\n    }\n  });\n}, {\n  threshold: 0.5 // Adjust the threshold to trigger when the card is 50% visible\n});\n\n// Observe each card\ncards.forEach(card => observer.observe(card));\n\n//# sourceURL=webpack://build/./src/assets/scripts/about/cardsNumber.js?");
+eval("document.addEventListener('DOMContentLoaded', () => {\n  const faqItems = document.querySelectorAll('.about-faq');\n  faqItems.forEach(item => {\n    const header = item.querySelector('.about-faq-header');\n    header.addEventListener('click', () => {\n      const isActive = item.classList.contains('active');\n      faqItems.forEach(i => i.classList.remove('active'));\n      if (!isActive) {\n        item.classList.add('active');\n      }\n    });\n  });\n});\n\n//# sourceURL=webpack://build/./src/assets/scripts/about/accordeon.js?");
 
 /***/ }),
 
@@ -37,18 +37,7 @@ eval("// Select all the cards and the number container\nconst cards = document.q
 /***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _swiper__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./swiper */ \"./src/assets/scripts/about/swiper.js\");\n/* harmony import */ var _cardsNumber__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./cardsNumber */ \"./src/assets/scripts/about/cardsNumber.js\");\n/* harmony import */ var _cardsNumber__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_cardsNumber__WEBPACK_IMPORTED_MODULE_1__);\n\n\n\n//# sourceURL=webpack://build/./src/assets/scripts/about/index.js?");
-
-/***/ }),
-
-/***/ "./src/assets/scripts/about/swiper.js":
-/*!********************************************!*\
-  !*** ./src/assets/scripts/about/swiper.js ***!
-  \********************************************/
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var swiper__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! swiper */ \"./node_modules/swiper/swiper.mjs\");\n/* harmony import */ var swiper_modules__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! swiper/modules */ \"./node_modules/swiper/modules/index.mjs\");\n\n\nswiper__WEBPACK_IMPORTED_MODULE_0__[\"default\"].use([swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Navigation, swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Controller, swiper_modules__WEBPACK_IMPORTED_MODULE_1__.EffectFade]);\nconst advantages = new swiper__WEBPACK_IMPORTED_MODULE_0__[\"default\"]('.about-advantages-swiper', {\n  slidesPerView: 1,\n  speed: 800,\n  loop: false,\n  effect: 'fade',\n  // Correctly setting the fade effect\n  fadeEffect: {\n    crossFade: true // Ensures smooth transition between slides\n  },\n  navigation: {\n    nextEl: '.about-advantages-swiper-button--next',\n    prevEl: '.about-advantages-swiper-button--prev'\n  },\n  breakpoints: {\n    360: {\n      slidesPerView: 1\n    }\n  }\n});\nif (window.innerWidth < 1023) {\n  const aboutSwiper = new swiper__WEBPACK_IMPORTED_MODULE_0__[\"default\"]('.about-infrastructure-list-wrap', {\n    slidesPerView: 1.1,\n    speed: 1500,\n    // loop: false,\n\n    navigation: {\n      nextEl: '.about-infrastructure-list-button--next',\n      prevEl: '.about-infrastructure-list-button--prev'\n    },\n    breakpoints: {\n      360: {\n        slidesPerView: 1.1,\n        spaceBetween: 10\n      },\n      768: {\n        slidesPerView: 2,\n        spaceBetween: 20\n      }\n    }\n  });\n}\n\n//# sourceURL=webpack://build/./src/assets/scripts/about/swiper.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _accordeon__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./accordeon */ \"./src/assets/scripts/about/accordeon.js\");\n/* harmony import */ var _accordeon__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_accordeon__WEBPACK_IMPORTED_MODULE_0__);\n\n\n//# sourceURL=webpack://build/./src/assets/scripts/about/index.js?");
 
 /***/ })
 
@@ -78,44 +67,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var swip
 /******/ 		return module.exports;
 /******/ 	}
 /******/ 	
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = __webpack_modules__;
-/******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/chunk loaded */
-/******/ 	!function() {
-/******/ 		var deferred = [];
-/******/ 		__webpack_require__.O = function(result, chunkIds, fn, priority) {
-/******/ 			if(chunkIds) {
-/******/ 				priority = priority || 0;
-/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
-/******/ 				deferred[i] = [chunkIds, fn, priority];
-/******/ 				return;
-/******/ 			}
-/******/ 			var notFulfilled = Infinity;
-/******/ 			for (var i = 0; i < deferred.length; i++) {
-/******/ 				var chunkIds = deferred[i][0];
-/******/ 				var fn = deferred[i][1];
-/******/ 				var priority = deferred[i][2];
-/******/ 				var fulfilled = true;
-/******/ 				for (var j = 0; j < chunkIds.length; j++) {
-/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every(function(key) { return __webpack_require__.O[key](chunkIds[j]); })) {
-/******/ 						chunkIds.splice(j--, 1);
-/******/ 					} else {
-/******/ 						fulfilled = false;
-/******/ 						if(priority < notFulfilled) notFulfilled = priority;
-/******/ 					}
-/******/ 				}
-/******/ 				if(fulfilled) {
-/******/ 					deferred.splice(i--, 1)
-/******/ 					var r = fn();
-/******/ 					if (r !== undefined) result = r;
-/******/ 				}
-/******/ 			}
-/******/ 			return result;
-/******/ 		};
-/******/ 	}();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	!function() {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -140,18 +92,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var swip
 /******/ 		};
 /******/ 	}();
 /******/ 	
-/******/ 	/* webpack/runtime/global */
-/******/ 	!function() {
-/******/ 		__webpack_require__.g = (function() {
-/******/ 			if (typeof globalThis === 'object') return globalThis;
-/******/ 			try {
-/******/ 				return this || new Function('return this')();
-/******/ 			} catch (e) {
-/******/ 				if (typeof window === 'object') return window;
-/******/ 			}
-/******/ 		})();
-/******/ 	}();
-/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	!function() {
 /******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
@@ -168,68 +108,12 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var swip
 /******/ 		};
 /******/ 	}();
 /******/ 	
-/******/ 	/* webpack/runtime/jsonp chunk loading */
-/******/ 	!function() {
-/******/ 		// no baseURI
-/******/ 		
-/******/ 		// object to store loaded and loading chunks
-/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
-/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
-/******/ 		var installedChunks = {
-/******/ 			"about": 0
-/******/ 		};
-/******/ 		
-/******/ 		// no chunk on demand loading
-/******/ 		
-/******/ 		// no prefetching
-/******/ 		
-/******/ 		// no preloaded
-/******/ 		
-/******/ 		// no HMR
-/******/ 		
-/******/ 		// no HMR manifest
-/******/ 		
-/******/ 		__webpack_require__.O.j = function(chunkId) { return installedChunks[chunkId] === 0; };
-/******/ 		
-/******/ 		// install a JSONP callback for chunk loading
-/******/ 		var webpackJsonpCallback = function(parentChunkLoadingFunction, data) {
-/******/ 			var chunkIds = data[0];
-/******/ 			var moreModules = data[1];
-/******/ 			var runtime = data[2];
-/******/ 			// add "moreModules" to the modules object,
-/******/ 			// then flag all "chunkIds" as loaded and fire callback
-/******/ 			var moduleId, chunkId, i = 0;
-/******/ 			if(chunkIds.some(function(id) { return installedChunks[id] !== 0; })) {
-/******/ 				for(moduleId in moreModules) {
-/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
-/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
-/******/ 					}
-/******/ 				}
-/******/ 				if(runtime) var result = runtime(__webpack_require__);
-/******/ 			}
-/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
-/******/ 			for(;i < chunkIds.length; i++) {
-/******/ 				chunkId = chunkIds[i];
-/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
-/******/ 					installedChunks[chunkId][0]();
-/******/ 				}
-/******/ 				installedChunks[chunkId] = 0;
-/******/ 			}
-/******/ 			return __webpack_require__.O(result);
-/******/ 		}
-/******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkbuild"] = self["webpackChunkbuild"] || [];
-/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
-/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
-/******/ 	}();
-/******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["vendors"], function() { return __webpack_require__("./src/assets/scripts/about.js"); })
-/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/assets/scripts/about.js");
 /******/ 	
 /******/ })()
 ;
